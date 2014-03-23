f56e7c4b3e0e8691f6a962ba0926c5d4fbb3103d
asdfdsas <occipite98@hotmail.it>
2014-03-23 18:34:55 +0100

    miglioramento character control 1

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -2,6 +2,14 @@ package mygame;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.bullet.BulletAppState;
+import com.jme3.input.KeyInput;
+import com.jme3.input.MouseInput;
+import com.jme3.input.controls.AnalogListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.input.controls.MouseAxisTrigger;
+import com.jme3.math.FastMath;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
@@ -30,22 +38,23 @@ public class Main extends SimpleApplication
        //inizializzazioni scena
        thread[0]=executor.submit(InitScene);
        thread[1]=executor.submit(InitPg);
+       thread[2]=executor.submit(InitKeys);
 //aspetta che i thread finiscano per attaccare gli spatial (se li attacchi nel thread c'è il rischio di crash)       
-       while(!thread[0].isDone() || !thread[1].isDone())
-          if(thread[0].isDone() && thread[1].isDone())
-          {
-           rootNode.attachChild(scena.SceneModel);
-           rootNode.attachChild(pg.model);
-          }
+       while(!thread[0].isDone() || !thread[1].isDone() || !thread[2].isDone()) {}
        
-       flyCam.setMoveSpeed(150f);
+       rootNode.attachChild(scena.SceneModel);
+       rootNode.attachChild(pg.model);
+          
+       flyCam.setEnabled(true);
+       Vector3f app=pg.model.getLocalTranslation();
+       cam.setLocation(new Vector3f(app.x,app.y+2,app.z-5));
+       cam.setRotation(pg.model.getLocalRotation());
     }
 
     @Override
     public void simpleUpdate(float tpf)
     {
-       // cam.setLocation(pg.control.getPhysicsLocation());
-       // System.out.println(pg.model.getLocalTranslation());
+      
     }
 
     @Override
@@ -71,6 +80,46 @@ public class Main extends SimpleApplication
         return null;
       }
     };
+    
+    private Callable InitKeys=new Callable() //thread che inizializza la mappa dei tasti
+    {
+       public Object call()
+       {
+           inputManager.addMapping("W",new KeyTrigger(KeyInput.KEY_W));
+           inputManager.addMapping("S",new KeyTrigger(KeyInput.KEY_S));
+           inputManager.addMapping("D",new KeyTrigger(KeyInput.KEY_D));
+           inputManager.addMapping("A",new KeyTrigger(KeyInput.KEY_A));
+           inputManager.addMapping("right",new MouseAxisTrigger(MouseInput.AXIS_X, true)); //movimento mouse verso destra
+           inputManager.addMapping("left",new MouseAxisTrigger(MouseInput.AXIS_X, false)); //movimento mouse verso sinistra
+           inputManager.addListener(PgMovement,"W","S","D","A","left","right");
+           return null;
+       }
+    };
+    
+    private AnalogListener PgMovement=new AnalogListener() //analog listener per il movimento del pg
+    {
+        public void onAnalog(String name, float value, float tpf) 
+        {
+           if(name.equals("right")) //rotazione braccia verso destra
+           {
+              if(pg.gradi+1.5<=360) pg.gradi+=1.5; else pg.gradi=0; //1.5 gradi -> 45°/30 358.5 -> 360-1.5
+              Quaternion quad=pg.model.getLocalRotation();
+              quad.fromAngleAxis(FastMath.PI*pg.gradi/180,Vector3f.UNIT_Y); //ruota il quaternione di pg.gradi lungo l'asse y
+              pg.model.setLocalRotation(quad);
+              pg.setCamera(cam);
+           } else
+           if(name.equals("left")) //rotazione braccia verso sinistra
+           {
+              if(pg.gradi-1.5>=0) pg.gradi-=0.75; else pg.gradi=360; 
+              Quaternion quad=pg.model.getLocalRotation();
+              quad.fromAngleAxis(FastMath.PI*pg.gradi/180,Vector3f.UNIT_Y); //ruota il quaternione di pg.gradi lungo l'asse y
+              pg.model.setLocalRotation(quad);
+              pg.setCamera(cam);
+           }
+        }          
+    };
+    
+
     
     @Override
     public void destroy()
--- src/mygame/Player.java
+++ src/mygame/Player.java
@@ -5,6 +5,7 @@ import com.jme3.bullet.BulletAppState;
 import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
 import com.jme3.bullet.control.CharacterControl;
 import com.jme3.math.Vector3f;
+import com.jme3.renderer.Camera;
 import com.jme3.scene.Spatial;
 
 
@@ -13,8 +14,12 @@ public class Player
   Spatial model; //modello 3d pg (braccia)
   CapsuleCollisionShape Shape;
   CharacterControl control;
+  int gradi;
+  
+  
    Player(AssetManager asset,BulletAppState bullet)
    {
+      gradi=0; 
       model=asset.loadModel("Models/birillomigliorato/birillo.migliorato.j3o");
       Shape=new CapsuleCollisionShape(1.5f, 6f); //primo parametro raggio,secondo altezza della capsula
       control=new CharacterControl(Shape,0.5f); //crea character control
@@ -30,4 +35,10 @@ public class Player
       model.setLocalTranslation(new Vector3f(app.x,app.y+Shape.getHeight()*3f/4,app.z)); 
    }
    
+   public void setCamera(Camera cam)
+   {
+      cam.setRotation(model.getLocalRotation());
+      Vector3f app=new Vector3f(0,-1f,-5);
+      cam.setLocation(model.localToWorld(app,app));
+   }   
 };
