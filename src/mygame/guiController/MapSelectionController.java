efb9c2e62239c3ec4514c9255484cb177df5d14f
krajetum <archimede96@hotmail.it>
2014-03-27 21:46:10 +0100

    gui potenziata che non funziona

--- src/mygame/guiController/MapSelectionController.java
+++ src/mygame/guiController/MapSelectionController.java
@@ -9,11 +9,15 @@ import com.jme3.app.Application;
 import com.jme3.app.SimpleApplication;
 import com.jme3.app.state.AbstractAppState;
 import com.jme3.app.state.AppStateManager;
+import com.jme3.asset.AssetManager;
+import com.jme3.input.FlyByCamera;
 import com.jme3.niftygui.NiftyJmeDisplay;
 import com.jme3.renderer.ViewPort;
+import com.jme3.scene.Node;
 import de.lessvoid.nifty.Nifty;
 import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.nifty.screen.ScreenController;
+import mygame.Main;
 
 
 
@@ -27,9 +31,22 @@ public class MapSelectionController extends AbstractAppState implements ScreenCo
     private NiftyJmeDisplay nifty;
     private ViewPort viewPort;
     private SimpleApplication app;
-    
+    public Main appl;
+    public Node rootNode2;
+    public FlyByCamera flycam;
+    public boolean menu=true;
 
-    MapSelectionController(AppStateManager stateManager, SimpleApplication app, ViewPort port) {
+    MapSelectionController(AppStateManager stateManager, 
+                        AssetManager man,
+                        SimpleApplication app, 
+                        ViewPort port,
+                        Main application,
+                        Node rootN,
+                        FlyByCamera fly) {
+        super.initialize(stateManager, app);
+        rootNode2=rootN;
+        flycam=fly;
+        appl=application;
         super.initialize(stateManager, app);
         this.app=(SimpleApplication)app;
         viewPort = port;
@@ -63,6 +80,23 @@ public class MapSelectionController extends AbstractAppState implements ScreenCo
         
     }
 
-    
+    public void start(int x, int y){
+        this.menu=false;  
+       //inizializzazioni scena
+       appl.thread[0]=appl.executor.submit(appl.InitScene);
+       appl.thread[1]=appl.executor.submit(appl.InitPg);
+       appl.thread[2]=appl.executor.submit(appl.InitKeys);
+       appl.thread[3]=appl.executor.submit(appl.InitVectorMob);
+      
+       //aspetta che i thread finiscano per attaccare gli spatial (se li attacchi nel thread c'è il rischio di crash)       
+       while(!appl.thread[0].isDone() || !appl.thread[1].isDone() || !appl.thread[2].isDone() || !appl.thread[3].isDone());
+       
+       rootNode2.attachChild(appl.scena.SceneModel);
+       rootNode2.attachChild(appl.pg.model[appl.pg.arma]);
+       flycam.setDragToRotate(false);
+       appl.thread[0]=appl.thread[1]=appl.thread[2]=null;
+       viewPort.removeProcessor(nifty); 
+       viewPort.removeProcessor(nifty);
+    }
 
 }
--- src/mygame/guiController/StartGUIController.java
+++ src/mygame/guiController/StartGUIController.java
@@ -47,7 +47,7 @@ public class StartGUIController extends AbstractAppState implements ScreenContro
         viewPort = port;
         optionController = new OptionGUIController(stateManager, app, viewPort);
         optionController.setNifty(nifty);
-        mapController = new MapSelectionController(stateManager, app, viewPort);
+        mapController = new MapSelectionController(stateManager, man ,app, viewPort, application, rootN, fly);
         mapController.setNifty(nifty);
         
     }    
@@ -82,30 +82,15 @@ public class StartGUIController extends AbstractAppState implements ScreenContro
         app.stop();
     }
     
-    @SuppressWarnings("empty-statement")
+   
     public void startGame(int x, int y){
-        /*
-       Screen screen = nifty.getNifty().getScreen("start");
-       screen.findElementByName("layer").startEffect(EffectEventId.onCustom);
-       */
+        viewPort.removeProcessor(nifty);
         
-        
-       this.menu=false;  
-       //inizializzazioni scena
-       appl.thread[0]=appl.executor.submit(appl.InitScene);
-       appl.thread[1]=appl.executor.submit(appl.InitPg);
-       appl.thread[2]=appl.executor.submit(appl.InitKeys);
-       appl.thread[3]=appl.executor.submit(appl.InitVectorMob);
-      
-       //aspetta che i thread finiscano per attaccare gli spatial (se li attacchi nel thread c'è il rischio di crash)       
-       while(!appl.thread[0].isDone() || !appl.thread[1].isDone() || !appl.thread[2].isDone() || !appl.thread[3].isDone());
-       
-       rootNode2.attachChild(appl.scena.SceneModel);
-       rootNode2.attachChild(appl.pg.model[appl.pg.arma]);
-       flycam.setDragToRotate(false);
-       appl.thread[0]=appl.thread[1]=appl.thread[2]=null;
-       
-       viewPort.removeProcessor(nifty);      
+        Nifty niftyOption = nifty.getNifty();
+        niftyOption.fromXml("Interface/mapSelection.xml", "start", mapController);
+        niftyOption.enableAutoScaling(Settings.system.getWidth(), Settings.system.getHeight());
+        mapController.setNifty(nifty);
+        viewPort.addProcessor(nifty);
     }
 
     public void bind(Nifty nifty, Screen screen) {
